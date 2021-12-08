48b45423d83ad60d6f5b43d22887034bc4bc2a4f
Adam Kaplan <adam.kaplan@redhat.com>
2021-12-08 14:28:02 -0500

    Use kubebuilder annotations for operator RBAC
    Use kubebuilder annotations for RBAC that is required by code explicitly
    invoked by the operator:
    
    - Replace namespace role/rolebinding with kubebuilder annotation
    - Replace TektonConfig role/rolebinding with kubebuilder annotation

--- controllers/shipwrightbuild_rbac.go
+++ controllers/shipwrightbuild_rbac.go
@@ -4,6 +4,7 @@ package controllers
 // allowed to modify named resources that deploy Shipwright Build.  This is especially true for the
 // cluster roles and custom resource definitions included in the release manifest.
 
+// +kubebuilder:rbac:groups=core,resources=namespaces,verbs=create;delete;get;list;patch;update;watch
 // +kubebuilder:rbac:groups=operator.shipwright.io,resources=shipwrightbuilds,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=operator.shipwright.io,resources=shipwrightbuilds/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=operator.shipwright.io,resources=shipwrightbuilds/finalizers,verbs=update
@@ -13,3 +14,4 @@ package controllers
 // +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterrolebindings,resourceNames=shipwright-build-controller,verbs=update;patch;delete
 // +kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=get;list;watch;create
 // +kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,resourceNames=builds.shipwright.io;buildruns.shipwright.io;buildstrategies.shipwright.io;clusterbuildstrategies.shipwright.io,verbs=update;patch;delete
+// +kubebuilder:rbac:groups=operator.tekton.dev,resources=tektonconfigs,verbs=create;delete;get;list;patch;update;watch
